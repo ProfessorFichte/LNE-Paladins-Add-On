ec125ccdb2b25a9795e6b6229af0b0b73c3609d2
ProfessorFichte <70310161+ProfessorFichte@users.noreply.github.com>
2025-01-16 21:12:37 +0100

    0.1.0 - 1.20.1 - Tweaks & Fixes for Beta

--- src/main/java/com/lne_paladins/item/weapons/SirensStaff.java
+++ src/main/java/com/lne_paladins/item/weapons/SirensStaff.java
@@ -1,6 +1,7 @@
 package com.lne_paladins.item.weapons;
 
 import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.annotation.Nullable;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ToolMaterial;
@@ -17,12 +18,16 @@ public class SirensStaff extends StaffItem {
     }
 
     public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
-        super.appendTooltip(stack, world, tooltip, context);
+
         tooltip.add(Text.translatable("lore.loot_n_explore.elder_guardian_weapon").formatted(Formatting.GOLD));
+        if(Screen.hasShiftDown()) {
         tooltip.add(Text.translatable("passive.lne_paladins.sirens_holy_staff_ally").formatted(Formatting.AQUA));
         tooltip.add(Text.translatable("passive.lne_paladins.sirens_holy_staff_ally_1").formatted(Formatting.AQUA));
         tooltip.add(Text.translatable("passive.lne_paladins.sirens_holy_staff_enemy").formatted(Formatting.AQUA));
         tooltip.add(Text.translatable("passive.lne_paladins.sirens_holy_staff_enemy_1").formatted(Formatting.AQUA));
-
+        }else{
+            tooltip.add(Text.translatable("tooltip.loot_n_explore.shift_down"));
+        }
+        super.appendTooltip(stack, world, tooltip, context);
     }
 }
